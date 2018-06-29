d5d1ec83a667c535abb8d584dd277fa958d6824d
mohammed-softordi <mohammed.ayowa@softordi.com>
2018-06-29 18:04:26 +0200

    icon-toggle polymer element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import '@webcomponents/webcomponentsjs/webcomponents-loader'
+import '@polymer/polymer/polymer-element';
+import '@polymer/iron-demo-helpers/demo-pages-shared-styles';
+import '@polymer/iron-demo-helpers/demo-snippet';
+import 'kse-common-ui/icon-toggle';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'App using KSE common icon component';
 }
